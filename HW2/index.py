892c20d948b4a30fa2f0f7a2e2f18402301f8cb3
Damian Rooney <dckrooney@gmail.com>
2011-10-09 21:26:28 -0700

    Huge amounts of stuff.

--- HW2/index.py
+++ HW2/index.py
@@ -1,15 +1,46 @@
 import os
+import sys
 import subprocess
+
+
+#pdfname = sys.argv[1]
+#indexfilename = sys.argv[2]
+#wordfilename = ""
+if len(sys.argv) > 2:
+	wordfilename = sys.argv[3]
+
+
+
+class Page:
+	num = 0
+	def __init__(self, text):
+		Page.num += 1
+		self.text = text
+		self.pagenum = Page.num
+
+	def findWord(self, word):
+		location = []
+		index = self.text[:].find(word)
+		location.append((index, index + len(word)))
+	
+		while index != -1:
+			index = self.text.find(word, index +len(word))
+			location.append((index, index + len(word)))
+
+		location.pop()
+		return location
+
 
 class Document:
 	
 	def __init__(self, pdfname):
-		subprocess.call("pdftotext %s temp.txt -layout" % (pdfname))
+		subprocess.call("pdftotext %s temp.txt -layout" % (pdfname), shell=True)
 		text = open("temp.txt", 'r')		
 		self.paginate(text)
 
 	def paginate(self, textfile):
 		self.body = textfile.readlines()
+		self.body = reduce(lambda x, y: x + y, self.body[:])			
 		self.tmppages = self.body.split('\x0c')
 		self.pages = {}
 		for text in self.tmppages:
@@ -17,45 +48,69 @@ class Document:
 			self.pages[newPg.pagenum] = newPg
 
 
-class Page:
-	num = 0
-	def __init__(self, text):
-		num += 1
-		self.text = text
-		self.pagenum = num
-
-	def findWord(self, word):
-		index = self.text[index:].find(word)
-		location = []
-		while index != -1:
-			index = page.text[index +len(word):].find(word)
-			location.append = (index, index + len(word))
-		return location
-
 class Index:
-	def __init__(self, document, wordfilename):
+	def __init__(self, document, filename):
 		self.location = {}
 		self.words = []
-		for word in self.words:
-			self.wordLocations[word] = {}
+		self.file = open(filename, 'r')
+		temp = self.file.readlines()
+		temp = map(lambda x: x.rstrip('\n'), temp)
+		
+		try:
+			pgnums = range(len(temp))
+			self.wordtext = range(len(temp))
+			for i in range(len(temp)):
+				[self.wordtext[i], pgnums[i]] = temp[i].split(" ")	
 
-		self.parseWordFile(wordfilename)
+			self.words = map(lambda x: tuple(x.split("/")), self.wordtext)
+			pgnumList = map(lambda x: x.split(","), pgnums)
+			pgnumList = reduce(lambda x, y: x + y, pgnumList)
+			pgnumList = map(lambda x: int(x), pgnumList)
 
-	def __init(self, index):
-		pass
+		except ValueError:
+			self.wordtext = temp[:]
+			self.words = map(lambda x: tuple(x.split("/")), self.wordtext)
+			pgnumList = 0
 
+		self.findWord(document, pgnumList)
+		self.writeOut()
+	
 	def parseWordFile(self, wordfilename):
 		self.wordfile = open(wordfilename, 'r')
-		self.wordtext = wordfile.readlines()
-		for line in self.wordtext:
-			wordtuple = tuple(line.split('/'))
-			self.words.append(wordtuple)
-	 	
-	def findWord(self, document):
+		self.wordtext = self.wordfile.readlines()
+		self.wordtext = map(lambda x: x.rstrip('\n'), self.wordtext)
+
+	def findWord(self, document, pageNum = 0):
+		if pageNum == 0:
+			pageNum = range(1, Page.num + 1)
+		print pageNum
 		for wordtuple in self.words:
-			for page in document.pages:
+			pagedict = {}
+			for num in pageNum:
+				print num
+				page = document.pages[num]
 				wordLocs = []
 				for word in wordtuple:
-					wordlocs = wordLocs + page.findWord(word)
+					temp = page.findWord(word)
+					wordLocs = wordLocs + temp
+				if wordLocs != []:
+					print wordLocs
 				pagedict[page.pagenum] = wordLocs
-		locations[wordtuple] = pagedict
+			self.location[wordtuple] = pagedict
+
+	def writeOut(self):
+		output = []
+		for wordtuple in self.words:
+			entry = "/".join(wordtuple)
+			entry += " "
+			for num, val in self.location[wordtuple].items():
+				if len(val) != 0:
+					entry += str(num) + ","
+			output.append(entry[:-1]) 
+		print output
+#		indexFile = open(indexfilename, 'w')
+#		indexFile.writelines(output)
+
+doc = Document("./Syllabus.pdf")
+print "created temp.txt"
+index = Index(doc, "wordfile.txt")
