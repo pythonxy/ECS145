a9eaea9647050a1875144084c2e427c20515088d
Damian Rooney <dckrooney@gmail.com>
2011-10-13 13:33:57 -0700

    Added exceptions for page scrolling

---
+++ HW2/Exc.py
@@ -0,0 +1,17 @@
+
+
+class PageDown(Exception):
+	def __init__(self):
+		pass
+
+class PageUp(Exception):
+	def __init__(self):
+		pass
+
+class EOF(Exception):
+	def __init__(self):
+		pass
+		
+class BOF(Exception):
+	def __init__(self):
+		pass
